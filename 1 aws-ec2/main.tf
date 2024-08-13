144b1104178362252b3310bc3af3267488d235c0
Uvesh <uveshrajwani786@gmail.com>
2024-08-13 19:06:26 +0530

    moved variable to a new file

--- 1 aws-ec2/main.tf
+++ 1 aws-ec2/main.tf
@@ -7,11 +7,7 @@ terraform {
   }
 }
 
-variable "region" {
-  default = "ap-south-1"
-  type = string   
-  description = "This is where AWS resources will be created in"
-}
+
 provider "aws" {
   region = var.region
 }
---
+++ 1 aws-ec2/variable.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  default = "ap-south-1"
+  type = string   
+  description = "This is where AWS resources will be created in"
+}
