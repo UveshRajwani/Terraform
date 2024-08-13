9b2232465c5fd3a97738616f2add851724a8c50e
Uvesh <uveshrajwani786@gmail.com>
2024-08-13 11:01:31 +0530

    new

---
+++ 1 aws-ec2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0ad21ae1d0696ad58"
+  instance_type = "t2.micro"
+  tags = {
+    "Made By" = "Terrform"
+    "Name" = "By Terrform"
+  }
+}
