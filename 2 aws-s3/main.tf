50a1cc2ad31a3a4d132411d8e1ffa84dd08a4f22
Uvesh <uveshrajwani786@gmail.com>
2024-08-22 18:09:49 +0530

    used a random id for bucket name and outputed the same value

--- 2 aws-s3/main.tf
+++ 2 aws-s3/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "5.62.0"
     }
+    random={
+        source = "hashicorp/random"
+        version = "3.6.2"
+    }
   }
 }
 
@@ -11,12 +15,21 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-
+provider "random" {
+  
+}
+resource "random_id" "Random-Bucket-Name" {
+byte_length = 20
+  
+}
 resource "aws_s3_bucket" "Demo-Bucket" {
-  bucket = "bucketbyterraformbyuvesh"
+  bucket = "bucketbyterraformbyuvesh-${random_id.Random-Bucket-Name.hex}"
 }
 resource "aws_s3_object" "File-To-Upload" {
     bucket = aws_s3_bucket.Demo-Bucket.bucket
     source = "./myfile.txt"
     key = "myfile.txt"
+}
+output "name" {
+  value = random_id.Random-Bucket-Name.hex
 }
